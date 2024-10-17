f0012c154cdebb8d344a6824348258be186db649
antonver <anton.verbovyi@gmail.com>
2024-10-18 00:26:05 +0200

    feat: Add tags, edit/delete, and toggle task completion
    Add support for tags, allowing users to categorize tasks.
    Implement edit, delete functionalities for tasks and tags.
    Include a feature to toggle the completion status of tasks.

--- list/views.py
+++ list/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-from django.urls import reverse
-from django.views.generic import ListView, UpdateView, DeleteView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, UpdateView, DeleteView, CreateView
 
-from list.models import Task
+from list.forms import TagForm, TaskForm
+from list.models import Task, Tag
 
 
 # Create your views here.
@@ -10,29 +11,55 @@ from list.models import Task
 class TaskListView(ListView):
     model = Task
 
+    def get_queryset(self):
+        return Task.objects.order_by('is_completed', '-date')
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "list/task_form.html"
+    success_url = reverse_lazy("list:task-list")
+
 
 class TaskUpdateView(UpdateView):
     model = Task
-    success_url = reverse("list:task_list")
-
+    form_class = TaskForm
+    template_name = "list/task_form.html"
+    success_url = reverse_lazy("list:task-list")
 
 class TaskDeleteView(DeleteView):
     model = Task
     template_name = "list/task_confirm_delete.html"
-    success_url = reverse("list:task_list")
+    success_url = reverse_lazy("list:task-list")
 
 
 class TagListView(ListView):
-    model = Task
-    context_object_name = "task_list"
-    template_name = "list/task_list.html"
+    model = Tag
+    context_object_name = "tag_list"
+    template_name = "list/tag_list.html"
 
+
+class TagCreateView(CreateView):
+    template_name = "list/tag_form.html"
+    model = Tag
+    success_url = reverse_lazy("list:tag-list")
+    form_class = TagForm
 
 class TagUpdateView(UpdateView):
-    model = Task
-    success_url = reverse("list:tag_list")
-
+    model = Tag
+    template_name = "list/tag_form.html"
+    success_url = reverse_lazy("list:tag-list")
+    form_class = TagForm
 
 class TagDeleteView(DeleteView):
-    model = Task
-    success_url = reverse("list:tag_list")
+    model = Tag
+    template_name = "list/tag_confirm_delete.html"
+    success_url = reverse_lazy("list:tag-list")
+
+
+def change(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = not task.is_completed
+    task.save()
+    return redirect("list:task-list")
